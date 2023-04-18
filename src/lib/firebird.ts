821cdd992812c529d6fa2567b00905170dda148d
OutBox Tecnologia <contato@outbox.inf.br>
2023-04-18 17:54:12 -0300

    .

--- src/lib/firebird.ts
+++ src/lib/firebird.ts
@@ -1,14 +1,14 @@
-export const options = {
-  host: '192.168.15.176',
-  port: 3050,
-  database: 'C:/Questor/Questor-original-06-07-2022-01-12-46.FDB',
-  user: 'sysdba',
-  password: 'masterkey',
-}
 // export const options = {
-//   host: '192.168.16.158',
+//   host: '192.168.15.176',
 //   port: 3050,
-//   database: '/Dados/base-oficial/nQuestor.FDB',
+//   database: 'C:/Questor/Questor-original-06-07-2022-01-12-46.FDB',
 //   user: 'sysdba',
-//   password: 'dbaarq16',
+//   password: 'masterkey',
 // }
+export const options = {
+  host: '192.168.16.158',
+  port: 3050,
+  database: '/Dados/base-oficial/nQuestor.FDB',
+  user: 'sysdba',
+  password: 'dbaarq16',
+}
