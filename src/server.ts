79f4cdf7b586e8c7b9e2123a77ea24db67ea466b
OutBox Tecnologia <contato@outbox.inf.br>
2023-04-26 12:00:59 -0300

    .

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
 import { app } from './app'
 import { env } from './env'
 
-const Versao =  require('../package.json').version
-
 app
   .listen({
     host: '0.0.0.0',
     port: env.PORT,
   })
-  .then(() => console.log('HTTP Server is running on version ' + Versao))
+  .then(() => console.log('HTTP Server is running on version ' + require('../package.json').version))
