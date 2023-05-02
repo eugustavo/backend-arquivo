0626c738f8849a28dd3a5b497b4908ee2376f361
OutBox Tecnologia <contato@outbox.inf.br>
2023-05-02 15:47:58 -0300

    .

--- src/env/index.ts
+++ src/env/index.ts
@@ -6,9 +6,9 @@ const envSchema = z.object({
   PORT: z.coerce.number().default(3000),
   FB_HOST: z.string().default('192.168.16.158'),
   FB_PORT: z.coerce.number().default(3050),
-  FB_DATABASE: z.string().default('C:/Questor/Questor-original-06-07-2022-01-12-46.FDB'),
+  FB_DATABASE: z.string().default('/Dados/base-oficial/nQuestor.FDB'),
   FB_USER: z.string().default('sysdba'),
-  FB_PASSWORD: z.string().default('masterkey'),
+  FB_PASSWORD: z.string().default('dbaarq16'),
   FB_LOWERCASE_KEYS: z.boolean().default(false),
   FB_RETRY_CONNECTION_INTERVAL: z.coerce.number().default(1000),
   FB_PAGE_SIZE: z.coerce.number().default(4096)
