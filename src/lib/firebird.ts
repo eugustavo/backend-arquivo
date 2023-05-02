6dbd82ba0a1fcd319742b881d3fa70a22f1b12c7
OutBox Tecnologia <contato@outbox.inf.br>
2023-05-02 15:58:35 -0300

    .

--- src/lib/firebird.ts
+++ src/lib/firebird.ts
@@ -1,12 +1,20 @@
-import { env } from './../env';
-
+// export const options = {
+//   host: '192.168.15.175',
+//   port: 3050,
+//   database: 'C:/Questor/Questor-original-06-07-2022-01-12-46.FDB',
+//   user: 'sysdba',
+//   password: 'masterkey',
+//   lowercase_keys: false,
+//   retryConnectionInterval: 1000,
+//   pageSize: 4096
+// }
 export const options = {
-  host: env.FB_HOST,
-  port: env.FB_HOST,
-  database: env.FB_DATABASE,
-  user: env.FB_USER,
-  password: env.FB_PASSWORD,
-  lowercase_keys: env.FB_LOWERCASE_KEYS,
-  retryConnectionInterval: env.FB_RETRY_CONNECTION_INTERVAL,
-  pageSize: env.FB_PAGE_SIZE
+  host: '192.168.16.158',
+  port: 3050,
+  database: '/Dados/base-oficial/nQuestor.FDB',
+  user: 'sysdba',
+  password: 'dbaarq16',
+  lowercase_keys: false,
+  retryConnectionInterval: 1000,
+  pageSize: 4096
 }
