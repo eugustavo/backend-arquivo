2e974b024d2788205edf8d2a755745bcd0443aba
OutBox Tecnologia <contato@outbox.inf.br>
2023-05-02 15:36:33 -0300

    .

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,6 +4,14 @@ import { z } from 'zod'
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'production', 'test']).default('dev'),
   PORT: z.coerce.number().default(3000),
+  FB_HOST: z.string().default('192.168.16.158'),
+  FB_PORT: z.coerce.number().default(3050),
+  FB_DATABASE: z.string().default('C:/Questor/Questor-original-06-07-2022-01-12-46.FDB'),
+  FB_USER: z.string().default('sysdba'),
+  FB_PASSWORD: z.string().default('masterkey'),
+  FB_LOWERCASE_KEYS: z.boolean().default(false),
+  FB_RETRY_CONNECTION_INTERVAL: z.coerce.number().default(1000),
+  FB_PAGE_SIZE: z.coerce.number().default(4096)
 })
 
 const _env = envSchema.safeParse(process.env)
