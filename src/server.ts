ba05ca9fc86abd48f64ced4e3746415ac4d06f9e
OutBox Tecnologia <contato@outbox.inf.br>
2023-05-03 10:29:58 -0300

    .

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import { app } from './app'
 import { env } from './env'
+var os = require("os");
+var hostname = os.hostname();
 
 app
   .listen({
     host: '0.0.0.0',
     port: env.PORT,
   })
-  .then(() => console.log('HTTP Server is running on version ' + require('../package.json').version))
+  .then(() => console.log('HTTP Server is running on version ' + require('../package.json').version) + ' at ' + hostname)
