72718792a8ddb1cb6cff2cd67101ce7126d49923
OutBox Tecnologia <contato@outbox.inf.br>
2023-05-03 10:33:36 -0300

    .

--- src/lib/firebird.ts
+++ src/lib/firebird.ts
@@ -1,16 +1,12 @@
 var os = require("os");
 var hostname = os.hostname();
 
-console.log(hostname)
-
-const dev = false
-
 export const options = {
-  host: dev ? '192.168.15.175' : '192.168.16.158',
+  host: hostname == 'MacBook-Pro-de-Bruno.local' ? '192.168.15.175' : '192.168.16.158',
   port: 3050,
-  database: dev ? 'C:/Questor/Questor-original-06-07-2022-01-12-46.FDB' : '/Dados/base-oficial/nQuestor.FDB',
+  database: hostname == 'MacBook-Pro-de-Bruno.local' ? 'C:/Questor/Questor-original-06-07-2022-01-12-46.FDB' : '/Dados/base-oficial/nQuestor.FDB',
   user: 'sysdba',
-  password: dev ? 'masterkey' : 'dbaarq16',
+  password: hostname == 'MacBook-Pro-de-Bruno.local' ? 'masterkey' : 'dbaarq16',
   lowercase_keys: false,
   retryConnectionInterval: 1000,
   pageSize: 4096
