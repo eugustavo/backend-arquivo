fca095c050177005e20edbfe06e114d65c4d2507
OutBox Tecnologia <contato@outbox.inf.br>
2023-05-25 10:50:51 -0300

    .

--- src/database/queries/lancamentos_bancarios/integrar_extrato.ts
+++ src/database/queries/lancamentos_bancarios/integrar_extrato.ts
@@ -1,9 +1,190 @@
 import firebird from 'node-firebird'
 import { options } from '../../../lib/firebird'
 
-export function query_extrato_insert(empresa: any, estab: any, conta: any, data: any, seq: any, numero: any, tipo: any, valor: any, descricao: any) {
+export function query_extrato_insert(empresa: any, estab: any, conta_ctb: any, data: any, seq: any, numero: any, tipo: any, valor: any, descricao: any) {
 
-  const sql = `EXECUTE PROCEDURE SIGRA_EXTRATO_INSERT('${empresa}', '${estab}', '${conta}', '${data}', '${seq}', '${numero}', '${tipo}', '${valor}', '${descricao}')`
+  const sql = `EXECUTE PROCEDURE SIGRA_EXTRATO_INSERT('${empresa}', '${estab}', '${conta_ctb}', '${data}', '${seq}', '${numero}', '${tipo}', '${valor}', '${descricao}')`
+
+  console.log(sql)
+
+  return new Promise((resolve, reject) => {
+    console.log('Chegou na Promise')
+    firebird.attach(options, function (err: any, db: any): any {
+      console.log('Chegou no Attach')
+      if (err) {
+        console.log('Chegou no Erro do Attach')
+        console.error('Erro na Conexão: ', err)
+        reject(err)
+      }
+      console.log('Chegou antes do DbQuery')
+      db.query(sql, function (err: any, result: any): any {
+        console.log('Chegou no DbQuery')
+        if (err) {
+          console.error('Erro na Query: ', err)
+          reject(err)
+        } else {
+          console.log('Chegou no Else do DbQuery')
+          resolve(result)
+        }
+        db.detach()
+      })
+    })
+  })
+}
+
+export function query_existe_conta_cadastrada(agencia: any, conta: any, digito: any, banco: any) {
+
+  const sql = `select * from CONTABANCARIA A WHERE A.CODIGOBANCO = '${banco}' AND A.NUMEROAGENCIA = '${agencia}' AND A.NUMEROCONTA = '${conta}' AND A.DIGITOCONTA = '${digito}'`
+
+  console.log(sql)
+
+  return new Promise((resolve, reject) => {
+    console.log('Chegou na Promise')
+    firebird.attach(options, function (err: any, db: any): any {
+      console.log('Chegou no Attach')
+      if (err) {
+        console.log('Chegou no Erro do Attach')
+        console.error('Erro na Conexão: ', err)
+        reject(err)
+      }
+      console.log('Chegou antes do DbQuery')
+      db.query(sql, function (err: any, result: any): any {
+        console.log('Chegou no DbQuery')
+        if (err) {
+          console.error('Erro na Query: ', err)
+          reject(err)
+        } else {
+          console.log('Chegou no Else do DbQuery')
+          resolve(result)
+        }
+        db.detach()
+      })
+    })
+  })
+}
+export function query_existe_agencia(agencia: any, banco: any, digito: any) {
+
+  const sql = `select * from AGENCIA A WHERE A.CODIGOBANCO = '${banco}' AND A.NUMEROAGENCIA = '${agencia}' AND A.DIGITOAGENCIA = '${digito}'`
+
+  console.log(sql)
+
+  return new Promise((resolve, reject) => {
+    console.log('Chegou na Promise')
+    firebird.attach(options, function (err: any, db: any): any {
+      console.log('Chegou no Attach')
+      if (err) {
+        console.log('Chegou no Erro do Attach')
+        console.error('Erro na Conexão: ', err)
+        reject(err)
+      }
+      console.log('Chegou antes do DbQuery')
+      db.query(sql, function (err: any, result: any): any {
+        console.log('Chegou no DbQuery')
+        if (err) {
+          console.error('Erro na Query: ', err)
+          reject(err)
+        } else {
+          console.log('Chegou no Else do DbQuery')
+          resolve(result)
+        }
+        db.detach()
+      })
+    })
+  })
+}
+export function query_seq_agencia() {
+
+  const sql = `SELECT MAX(CODIGOAGENCIABANC)+1 AS SEQ FROM AGENCIA `
+
+  console.log(sql)
+
+  return new Promise((resolve, reject) => {
+    console.log('Chegou na Promise')
+    firebird.attach(options, function (err: any, db: any): any {
+      console.log('Chegou no Attach')
+      if (err) {
+        console.log('Chegou no Erro do Attach')
+        console.error('Erro na Conexão: ', err)
+        reject(err)
+      }
+      console.log('Chegou antes do DbQuery')
+      db.query(sql, function (err: any, result: any): any {
+        console.log('Chegou no DbQuery')
+        if (err) {
+          console.error('Erro na Query: ', err)
+          reject(err)
+        } else {
+          console.log('Chegou no Else do DbQuery')
+          resolve(result)
+        }
+        db.detach()
+      })
+    })
+  })
+}
+export function query_seq_conta() {
+
+  const sql = `SELECT MAX(CODIGOCONTABANCARIA)+1 AS SEQ FROM CONTABANCARIA`
+
+  console.log(sql)
+
+  return new Promise((resolve, reject) => {
+    console.log('Chegou na Promise')
+    firebird.attach(options, function (err: any, db: any): any {
+      console.log('Chegou no Attach')
+      if (err) {
+        console.log('Chegou no Erro do Attach')
+        console.error('Erro na Conexão: ', err)
+        reject(err)
+      }
+      console.log('Chegou antes do DbQuery')
+      db.query(sql, function (err: any, result: any): any {
+        console.log('Chegou no DbQuery')
+        if (err) {
+          console.error('Erro na Query: ', err)
+          reject(err)
+        } else {
+          console.log('Chegou no Else do DbQuery')
+          resolve(result)
+        }
+        db.detach()
+      })
+    })
+  })
+}
+export function query_cadastra_agencia(banco: any, agencia: any, digito: any, seq: any) {
+
+  const sql = `INSERT INTO AGENCIA (CODIGOBANCO, NUMEROAGENCIA, DIGITOAGENCIA, NOMEAGENCIA, CODIGOAGENCIABANC) VALUES ('${banco}', '${agencia}', '${digito}', 'Importação Sigra', '${seq}')`
+
+  console.log(sql)
+
+  return new Promise((resolve, reject) => {
+    console.log('Chegou na Promise')
+    firebird.attach(options, function (err: any, db: any): any {
+      console.log('Chegou no Attach')
+      if (err) {
+        console.log('Chegou no Erro do Attach')
+        console.error('Erro na Conexão: ', err)
+        reject(err)
+      }
+      console.log('Chegou antes do DbQuery')
+      db.query(sql, function (err: any, result: any): any {
+        console.log('Chegou no DbQuery')
+        if (err) {
+          console.error('Erro na Query: ', err)
+          reject(err)
+        } else {
+          console.log('Chegou no Else do DbQuery')
+          resolve(result)
+        }
+        db.detach()
+      })
+    })
+  })
+}
+export function query_cadastra_conta(codigo: any, banco: any, agencia: any, conta: any, digito: any) {
+
+  const sql = `INSERT INTO CONTABANCARIA (CODIGOCONTABANCARIA, CODIGOBANCO, NUMEROAGENCIA, NUMEROCONTA, DIGITOCONTA) VALUES ('${codigo}', '${banco}', '${agencia}', '${conta}', '${digito}')`
 
   console.log(sql)
 
--- src/http/controllers/lancamentos_bancarios/integrar_extrato.ts
+++ src/http/controllers/lancamentos_bancarios/integrar_extrato.ts
@@ -1,7 +1,16 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 
-import { query_extrato_insert } from '@/database/queries/lancamentos_bancarios/integrar_extrato'
+import {
+    query_extrato_insert,
+    query_existe_conta_cadastrada,
+    query_existe_agencia,
+    query_cadastra_conta,
+    query_seq_agencia,
+    query_seq_conta,
+    query_cadastra_agencia
+
+} from '@/database/queries/lancamentos_bancarios/integrar_extrato'
 
 export async function extrato_insert(
     request: FastifyRequest,
@@ -10,19 +19,62 @@ export async function extrato_insert(
     const bodySchema = z.object({
         empresa: z.any(),
         estab: z.any(),
-        conta: z.any(),
+        conta_ctb: z.any(),
         data: z.any(),
         seq: z.any(),
         numero: z.any(),
         tipo: z.any(),
         valor: z.any(),
         descricao: z.any(),
+        dados_conta_agencia: z.any(),
+        dados_conta_agencia_digito: z.any(),
+        dados_conta_conta: z.any(),
+        dados_conta_conta_digito: z.any(),
+        dados_conta_banco: z.any(),
 
     })
 
-    const { empresa, estab, conta, data, seq, numero, tipo, valor, descricao } = bodySchema.parse(request.body)
+    const { empresa, estab, conta_ctb, data, seq, numero, tipo, valor, descricao, dados_conta_agencia, dados_conta_agencia_digito, dados_conta_conta, dados_conta_conta_digito, dados_conta_banco } = bodySchema.parse(request.body)
 
-    await query_extrato_insert(empresa, estab, conta, data, seq, numero, tipo, valor, descricao)
+    const existeConta: any = await query_existe_conta_cadastrada(dados_conta_agencia, dados_conta_conta, dados_conta_conta_digito, dados_conta_banco)
+
+    if (existeConta.rowCount > 0) {
+
+        await query_extrato_insert(empresa, estab, conta_ctb, data, seq, numero, tipo, valor, descricao)
+
+    } else {
+
+        const existeAgencia: any = await query_existe_agencia(dados_conta_agencia, dados_conta_banco, dados_conta_agencia_digito)
+
+        if (existeAgencia.rowCount == 0) {
+
+            const getSeq: any = await query_seq_agencia()
+
+            await query_cadastra_agencia(dados_conta_banco, dados_conta_agencia, dados_conta_agencia_digito, getSeq[0].seq)
+
+            const existeAgencia: any = await query_existe_agencia(dados_conta_agencia, dados_conta_banco, dados_conta_agencia_digito)
+
+            if (existeAgencia.rowCount > 0) {
+                const getSeq: any = await query_seq_conta()
+
+                await query_cadastra_conta(getSeq[0].seq, dados_conta_banco, existeAgencia.rows[0].numeroagencia, dados_conta_conta, dados_conta_conta_digito)
+
+                await query_extrato_insert(empresa, estab, conta_ctb, data, seq, numero, tipo, valor, descricao)
+            }
+
+        } else {
+
+            const getSeq: any = await query_seq_conta()
+
+            await query_cadastra_conta(getSeq[0].seq, dados_conta_banco, existeAgencia.rows[0].numeroagencia, dados_conta_conta, dados_conta_conta_digito)
+
+            await query_extrato_insert(empresa, estab, conta_ctb, data, seq, numero, tipo, valor, descricao)
+
+        }
+
+    }
+
+
 
     reply.send({
         message: 'ok'
