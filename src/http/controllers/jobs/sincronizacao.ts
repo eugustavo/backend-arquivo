1eb42d2ddbf3df11e3d4a7e82cad675e00d7589c
OutBox Tecnologia <contato@outbox.inf.br>
2023-05-25 17:07:17 -0300

    .

--- src/http/controllers/jobs/sincronizacao.ts
+++ src/http/controllers/jobs/sincronizacao.ts
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
 import { query_sinc_funcionarios } from '@/database/queries/sincronizacao/sinc_funcionarios'
+import { query_sinc_empresas } from '@/database/queries/sincronizacao/sinc_empresas'
+import { query_sinc_contas_ctb_bancos } from '@/database/queries/sincronizacao/sinc_contas_ctb_bancos'
 
 export async function job_sinc_funcionarios() {
 
@@ -35,6 +37,98 @@ export async function job_sinc_funcionarios() {
 
                 } else {
                     console.log('Erro ao Incluir Funcionário')
+                }
+
+            })
+            .catch(function (error) {
+                console.log('Falha no Processo:', error)
+
+            })
+
+    }
+
+}
+export async function job_sinc_empresas() {
+
+    console.log('Iniciando Sincronização Agendada de Empresas')
+
+    const listaSincronizar: any = await query_sinc_empresas()
+
+    console.log('Total de Empresas para Sincronizar: ' + listaSincronizar.length)
+
+    for (let i = 0; i < listaSincronizar.length; i++) {
+
+        axios.post('https://api.aws.inf.br/connect/questor/empresas/incluir',
+            {
+                id: listaSincronizar[i].CODIGOEMPRESA,
+                nome: listaSincronizar[i].FANTASIA,
+                razao: listaSincronizar[i].RAZAO,
+                cnpj: listaSincronizar[i].CNPJ,
+                empresa: listaSincronizar[i].QUESTOR_EMPRESA,
+                estabelecimento: listaSincronizar[i].QUESTOR_ESTABELECIMENTO,
+                datacad: listaSincronizar[i].DATA_CAD,
+                endereco: listaSincronizar[i].TIPO + ' ' + listaSincronizar[i].ENDERECO,
+                numero: listaSincronizar[i].NUMERO,
+                bairro: listaSincronizar[i].BAIRRO,
+                cidade: listaSincronizar[i].CIDADE,
+                uf: listaSincronizar[i].UF
+            },
+            {
+                headers: {
+                    contenType: 'application/json'
+                }
+            })
+
+            .then(function (response) {
+
+                if (response.status === 201) {
+
+                    console.log('Empresa Incluída com Sucesso')
+
+                } else {
+                    console.log('Erro ao Incluir Empresa')
+                }
+
+            })
+            .catch(function (error) {
+                console.log('Falha no Processo:', error)
+
+            })
+
+    }
+
+}
+export async function job_sinc_contas_banco_ctb() {
+
+    console.log('Iniciando Sincronização Agendada de Contas Contábeis Bancárias')
+
+    const listaSincronizar: any = await query_sinc_contas_ctb_bancos()
+
+    console.log('Total de Contas Contábeis Bancárias para Sincronizar: ' + listaSincronizar.length)
+
+    for (let i = 0; i < listaSincronizar.length; i++) {
+
+        axios.post('https://api.aws.inf.br/connect/questor/contas_bancos_ctb/incluir',
+            {
+                empresa: listaSincronizar[i].CODIGOEMPRESA,
+                estab: listaSincronizar[i].CODIGOESTAB,
+                conta: listaSincronizar[i].CONTACTB,
+                descricao: listaSincronizar[i].DESCRCONTA,
+            },
+            {
+                headers: {
+                    contenType: 'application/json'
+                }
+            })
+
+            .then(function (response) {
+
+                if (response.status === 201) {
+
+                    console.log('Conta Contábel Bancária Incluída com Sucesso')
+
+                } else {
+                    console.log('Erro ao Incluir Conta Contábel Bancária')
                 }
 
             })
--- src/server.ts
+++ src/server.ts
@@ -3,16 +3,26 @@ import { env } from './env'
 import schedule from 'node-schedule';
 import moment from 'moment';
 
-import { job_sinc_funcionarios } from './http/controllers/jobs/sincronizacao';
+import {
+  job_sinc_funcionarios,
+  job_sinc_empresas,
+  job_sinc_contas_banco_ctb
+} from './http/controllers/jobs/sincronizacao';
 
 var os = require("os");
 var hostname = os.hostname();
 
 
-const jobSincronizacao = schedule.scheduleJob('0 45 16 * * *', async function () {
+const jobSincronizacao = schedule.scheduleJob('0 0 22 * * *', async function () {
   console.log('Iniciando Job Agendado de Sincronização de Funcionários em ' + moment().format('DD/MM/YYYY HH:mm:ss'));
   await job_sinc_funcionarios()
   console.log('Finalizando Job Agendado de Sincronização de Funcionários em ' + moment().format('DD/MM/YYYY HH:mm:ss'));
+  console.log('Iniciando Job Agendado de Sincronização de Empresas em ' + moment().format('DD/MM/YYYY HH:mm:ss'));
+  await job_sinc_empresas()
+  console.log('Finalizando Job Agendado de Sincronização de Empresas em ' + moment().format('DD/MM/YYYY HH:mm:ss'));
+  console.log('Iniciando Job Agendado de Sincronização de Contas Contábeis Bancárias em ' + moment().format('DD/MM/YYYY HH:mm:ss'));
+  await job_sinc_contas_banco_ctb()
+  console.log('Finalizando Job Agendado de Sincronização de Contas Contábeis Bancárias em ' + moment().format('DD/MM/YYYY HH:mm:ss'));
 
 });
 
