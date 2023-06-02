16395863fb2b2bd48fd0e157147a0031504bcd3a
OutBox Tecnologia <contato@outbox.inf.br>
2023-06-02 09:47:36 -0300

    .

--- src/database/queries/notas_nao_lancadas/nfs_analista.ts
+++ src/database/queries/notas_nao_lancadas/nfs_analista.ts
@@ -13,29 +13,22 @@ export function query_nfs_analista(data1: any, data2: any, analista: any) {
   console.log(sql)
 
   return new Promise((resolve, reject) => {
-    console.log('Chegou na Promise')
     firebird.attach(options, function (err: any, db: any): any {
-      console.log('Chegou no Attach')
       if (err) {
-        console.log('Chegou no Erro do Attach')
         console.error('Erro na Conexão: ', err)
         reject(err)
-        return
       }
       console.log('Chegou antes do DbQuery')
       db.query(sql, function (err: any, result: any): any {
-        console.log('Chegou no DbQuery')
         if (err) {
           console.error('Erro na Query: ', err)
           reject(err)
-          return
         } else {
-          console.log('Chegou no Else do DbQuery')
           console.log(result.length)
           resolve(result)
         }
+        db.detach()
       })
-      db.detach()
     })
   })
 }
