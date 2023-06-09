80c17bb8e794eee9ec7dd0b0b03a2cd655105efe
OutBox Tecnologia <contato@outbox.inf.br>
2023-06-09 11:34:08 -0300

    .

--- src/database/queries/cmi/simples_nacional/analistas.ts
+++ src/database/queries/cmi/simples_nacional/analistas.ts
@@ -7,7 +7,7 @@ export function Query_CMI_Simples_Analistas() {
         A.CODIGOEMPRESA,
         A.CODIGOESTAB,
         A.INSCRFEDERAL,
-        A.NOMEESTABCOMPLETO,
+        cast(A.NOMEESTABCOMPLETO as varchar(120) character set win1252) as NOMEESTAB
         B.CODIGOUSUARIO,
         B.NOMEUSUARIO
         from  estab a left join usuario b on (a.CVMAUDITOR = B.CODIGOUSUARIO)
